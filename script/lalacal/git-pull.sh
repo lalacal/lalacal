2faaa182025a012b8eb82e884dc44b88da24c9ff
Troy Will <troydwill@gmail.com>
2009-07-09 16:45:02 -0700

    minor

--- script/lalacal/git-pull.sh
+++ script/lalacal/git-pull.sh
@@ -2,4 +2,7 @@
 set -o errexit
 APP_PATH='/home/rails/lalacal'
 cd ${APP_PATH}
-git pull
+echo `date` >> git-pull-test1
+/usr/local/bin/git pull > output1
+/usr/local/bin/git pull 2>output.error
+echo `date` >> git-pull-test2
